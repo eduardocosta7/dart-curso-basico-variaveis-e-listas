deef08671556cd9b789f50b465321a637c1d02fe
eduardocosta7 <eduardo.f.costa@live.com>
2023-10-03 22:21:26 -0300

    A02 - listas e tipos de variaveis

--- bin/projeto_curso_dart.dart
+++ bin/projeto_curso_dart.dart
@@ -3,10 +3,24 @@ import 'dart:ffi';
 import 'package:projeto_curso_dart/projeto_curso_dart.dart' as projeto_curso_dart;
 
 void main() {
-  testString();
-  testInt();
-  testDouble();
-  testBoolean();
+  testListAndVariablesType();
+}
+
+void testListAndVariablesType() {
+  const String nome = "Eduardo";
+  var idade = 24;
+  var altura = 1.86;
+  final ehGeek;
+
+  ehGeek = true;
+
+  List<dynamic> eduardo = [nome, idade, altura, ehGeek];
+
+  String frase = "Eu sou o ${eduardo[0]}, "
+      "tenho ${eduardo[1]} anos de idade"
+      "e ${eduardo[2]} de altura.";
+
+  print(frase);
 }
 
 void testString() {
