7def99e11e3eb38a5f225514811a5b7ac817af56
eduardocosta7 <eduardo.f.costa@live.com>
2023-10-03 22:51:25 -0300

    A03 - utilizando condicoes e lacos de repeticao

--- bin/projeto_curso_dart.dart
+++ bin/projeto_curso_dart.dart
@@ -1,9 +1,49 @@
-import 'dart:ffi';
-
-import 'package:projeto_curso_dart/projeto_curso_dart.dart' as projeto_curso_dart;
-
 void main() {
-  testListAndVariablesType();
+  testConditions();
+}
+
+void testConditions() {
+  const String nome = "Eduardo";
+  var idade = 24;
+  var altura = 1.86;
+  final ehGeek;
+  ehGeek = true;
+  final bool maiorDeIdade;
+  maiorDeIdade = idade >= 18;
+  bool participouDaCorrida = false;
+  int numeroDeVoltas = 0;
+  int energia = 100;
+
+  if (maiorDeIdade) {
+    participouDaCorrida = true;
+  }
+
+  if (participouDaCorrida) {
+    for (int i = 1; i < 6; i++) {
+      numeroDeVoltas++;
+    }
+  }
+
+  while (energia > 0) {
+    print("Mais um repetição");
+    energia = energia - 5;
+  }
+
+  do {
+    print("Mais um repetição");
+    energia = energia - 5;
+  } while (energia > 0);
+
+  List<dynamic> eduardo = [nome, idade, altura, ehGeek];
+
+  String frase = "Eu sou o ${eduardo[0]},\n"
+      "tenho ${eduardo[1]} anos de idade\n"
+      "e ${eduardo[2]} de altura.\n"
+      "Eu sou maior de idade? $maiorDeIdade\n"
+      "Participou da corriada? $participouDaCorrida\n"
+      "Número de voltas: $numeroDeVoltas";
+
+  print(frase);
 }
 
 void testListAndVariablesType() {
@@ -31,8 +71,7 @@ void testString() {
 
 void testBoolean() {
   bool vcEhGeek = true;
-  if(vcEhGeek)
-    print('Você ganhou uma camisa!');
+  if (vcEhGeek) print('Você ganhou uma camisa!');
 }
 
 void testDouble() {
