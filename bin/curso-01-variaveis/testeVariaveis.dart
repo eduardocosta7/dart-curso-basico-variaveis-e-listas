5f7cddb3a75aa69f36b6133b1b90f4d8588f12f5
eduardocosta7 <eduardo.f.costa@live.com>
2023-10-12 19:06:14 -0300

    A04 - Criando objetos

---
+++ bin/curso-01-variaveis/testeVariaveis.dart
@@ -0,0 +1,81 @@
+void testConditions() {
+  const String nome = "Eduardo";
+  var idade = 24;
+  var altura = 1.86;
+  final ehGeek;
+  ehGeek = true;
+  final bool maiorDeIdade;
+  maiorDeIdade = idade >= 18;
+  bool participouDaCorrida = false;
+  int numeroDeVoltas = 0;
+  int energia = 100;
+
+  if (maiorDeIdade) {
+    participouDaCorrida = true;
+  }
+
+  if (participouDaCorrida) {
+    for (int i = 1; i < 6; i++) {
+      numeroDeVoltas++;
+    }
+  }
+
+  while (energia > 0) {
+    print("Mais um repetição");
+    energia = energia - 5;
+  }
+
+  do {
+    print("Mais um repetição");
+    energia = energia - 5;
+  } while (energia > 0);
+
+  List<dynamic> eduardo = [nome, idade, altura, ehGeek];
+
+  String frase = "Eu sou o ${eduardo[0]},\n"
+      "tenho ${eduardo[1]} anos de idade\n"
+      "e ${eduardo[2]} de altura.\n"
+      "Eu sou maior de idade? $maiorDeIdade\n"
+      "Participou da corriada? $participouDaCorrida\n"
+      "Número de voltas: $numeroDeVoltas";
+
+  print(frase);
+}
+
+void testListAndVariablesType() {
+  const String nome = "Eduardo";
+  var idade = 24;
+  var altura = 1.86;
+  final ehGeek;
+
+  ehGeek = true;
+
+  List<dynamic> eduardo = [nome, idade, altura, ehGeek];
+
+  String frase = "Eu sou o ${eduardo[0]}, "
+      "tenho ${eduardo[1]} anos de idade"
+      "e ${eduardo[2]} de altura.";
+
+  print(frase);
+}
+
+void testString() {
+  String nome = 'Eduardo';
+  String sobreNome = 'Costa';
+  print("$nome $sobreNome");
+}
+
+void testBoolean() {
+  bool vcEhGeek = true;
+  if (vcEhGeek) print('Você ganhou uma camisa!');
+}
+
+void testDouble() {
+  double altura = 1.85;
+  print('A altura é: $altura');
+}
+
+void testInt() {
+  int idade = 24;
+  print('A idade é: $idade!');
+}
--- bin/projeto_curso_dart.dart
+++ bin/projeto_curso_dart.dart
@@ -1,85 +1,24 @@
 void main() {
-  testConditions();
+  Fruta banana = Fruta("Banana", 80.0, "Amarela", "Doce", 20);
+  banana.estaMadura(30);
 }
 
-void testConditions() {
-  const String nome = "Eduardo";
-  var idade = 24;
-  var altura = 1.86;
-  final ehGeek;
-  ehGeek = true;
-  final bool maiorDeIdade;
-  maiorDeIdade = idade >= 18;
-  bool participouDaCorrida = false;
-  int numeroDeVoltas = 0;
-  int energia = 100;
+class Fruta {
+  String nome;
+  double peso;
+  String cor;
+  String sabor;
+  int diasDesdeColheita;
+  bool? isMadura;
 
-  if (maiorDeIdade) {
-    participouDaCorrida = true;
-  }
+  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasDesdeColheita, {this.isMadura});
 
-  if (participouDaCorrida) {
-    for (int i = 1; i < 6; i++) {
-      numeroDeVoltas++;
+  estaMadura(int diasParaMadura) {
+    isMadura = diasParaMadura >= diasDesdeColheita;
+    if (isMadura!) {
+      print("Está madura!");
+    } else {
+      print("Não está madura!");
     }
   }
-
-  while (energia > 0) {
-    print("Mais um repetição");
-    energia = energia - 5;
-  }
-
-  do {
-    print("Mais um repetição");
-    energia = energia - 5;
-  } while (energia > 0);
-
-  List<dynamic> eduardo = [nome, idade, altura, ehGeek];
-
-  String frase = "Eu sou o ${eduardo[0]},\n"
-      "tenho ${eduardo[1]} anos de idade\n"
-      "e ${eduardo[2]} de altura.\n"
-      "Eu sou maior de idade? $maiorDeIdade\n"
-      "Participou da corriada? $participouDaCorrida\n"
-      "Número de voltas: $numeroDeVoltas";
-
-  print(frase);
-}
-
-void testListAndVariablesType() {
-  const String nome = "Eduardo";
-  var idade = 24;
-  var altura = 1.86;
-  final ehGeek;
-
-  ehGeek = true;
-
-  List<dynamic> eduardo = [nome, idade, altura, ehGeek];
-
-  String frase = "Eu sou o ${eduardo[0]}, "
-      "tenho ${eduardo[1]} anos de idade"
-      "e ${eduardo[2]} de altura.";
-
-  print(frase);
-}
-
-void testString() {
-  String nome = 'Eduardo';
-  String sobreNome = 'Costa';
-  print("$nome $sobreNome");
-}
-
-void testBoolean() {
-  bool vcEhGeek = true;
-  if (vcEhGeek) print('Você ganhou uma camisa!');
-}
-
-void testDouble() {
-  double altura = 1.85;
-  print('A altura é: $altura');
-}
-
-void testInt() {
-  int idade = 24;
-  print('A idade é: $idade!');
 }
