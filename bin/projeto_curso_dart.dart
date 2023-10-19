2e155b90434a8e34c80b39a9b1a4848142b2b085
eduardocosta7 <eduardo.f.costa@live.com>
2023-10-18 22:43:46 -0300

    A05 - Entendendo herança

--- bin/projeto_curso_dart.dart
+++ bin/projeto_curso_dart.dart
@@ -1,17 +1,27 @@
 void main() {
   Fruta banana = Fruta("Banana", 80.0, "Amarela", "Doce", 20);
-  banana.estaMadura(30);
+  banana.printAlimento();
 }
 
-class Fruta {
+class Alimento {
   String nome;
   double peso;
   String cor;
   String sabor;
+
+  Alimento(this.nome, this.peso, this.cor, this.sabor);
+
+  void printAlimento() {
+    print('Este(a) $nome pesa $peso gramas e tem a cor $cor');
+  }
+}
+
+class Fruta extends Alimento {
   int diasDesdeColheita;
   bool? isMadura;
 
-  Fruta(this.nome, this.peso, this.cor, this.sabor, this.diasDesdeColheita, {this.isMadura});
+  Fruta(nome, peso, cor, sabor, this.diasDesdeColheita, {this.isMadura})
+      : super(nome, peso, cor, sabor);
 
   estaMadura(int diasParaMadura) {
     isMadura = diasParaMadura >= diasDesdeColheita;
